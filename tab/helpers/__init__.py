696fc787f89436efdd659560221e2035c24c98bb
P. R. d. O <d.ol.rod@protonmail.com>
2022-01-11 16:44:20 -0600

    PEP8 fixes

--- tab/helpers/__init__.py
+++ tab/helpers/__init__.py
@@ -1,4 +1,5 @@
 from tab.helpers.argparse import parse_parameters
 from tab.helpers.yaml import load_yaml
-from tab.helpers.functionality import transform_streamers_to_dict, post_to_twitter, \
-     generate_tweet_message, check_finished_streams, check_started_streams
+from tab.helpers.functionality import transform_streamers_to_dict, \
+     post_to_twitter, generate_tweet_message, check_finished_streams, \
+     check_started_streams
