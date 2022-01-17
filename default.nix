7ee3f256836529f098e3b7a6147f0ca523e0f56b
P. R. d. O <d.ol.rod@tutanota.com>
2022-01-16 22:22:07 -0600

    Adding version number

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@ let
 in
 requirements.pythonPackages.buildPythonApplication {
   pname = "stream-alert-bot";
-  version = "0.1.2";
+  version = "0.2";
   format = "pyproject";
 
   src = lib.cleanSource ./.;
