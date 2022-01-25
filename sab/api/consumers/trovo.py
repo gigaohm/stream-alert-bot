24587d38357fd0054a7685dbe3fe862967060cd3
P. R. d. O <d.ol.rod@tutanota.com>
2022-01-25 12:58:20 -0600

    Catching TrovoApiException

--- sab/api/consumers/trovo.py
+++ sab/api/consumers/trovo.py
@@ -1,7 +1,7 @@
 import sys
 from logging import Logger, getLogger
 from requests.exceptions import ConnectionError
-from trovoApi import TrovoClient
+from trovoApi import TrovoClient, TrovoApiException
 
 
 class TrovoConsumer:
@@ -17,9 +17,14 @@ class TrovoConsumer:
             self.__logger = getLogger("stream-alert-bot/api/consumers/trovo")
             self.__logger.debug("Connection to Trovo created")
             self.client = trovo
+        except TrovoApiException as tr_e:
+            self.__logger.error("".join(["Trovo API gave the following ",
+                                         "error: ", tr_e.message]))
+            sys.exit(1)
         except ConnectionError as c_e:
             self.__logger.error(("Not able to connect to API. Verify your "
                                  "network"))
+            sys.exit(1)
         except Exception as e:
             self.__logger.exception(e)
             sys.exit(1)
@@ -33,6 +38,10 @@ class TrovoConsumer:
             self.__logger.debug("Obtained streamers info:")
             self.__logger.debug(streamers_info)
             return streamers_info["users"]
+        except TrovoApiException as tr_e:
+            self.__logger.error("".join(["Trovo API gave the following ",
+                                         "error: ", tr_e.message]))
+            sys.exit(1)
         except ConnectionError as c_e:
             self.__logger.error(("Not able to connect to API. Verify your "
                                  "network."))
@@ -55,6 +64,10 @@ class TrovoConsumer:
             self.__logger.debug("Obtained channels status:")
             self.__logger.debug(statuses)
             return statuses
+        except TrovoApiException as tr_e:
+            self.__logger.error("".join(["Trovo API gave the following ",
+                                         "error: ", tr_e.message]))
+            sys.exit(1)
         except ConnectionError as c_e:
             self.__logger.error(("Not able to connect to API. Verify your "
                                  "network."))
