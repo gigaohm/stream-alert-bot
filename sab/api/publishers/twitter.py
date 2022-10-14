9775f4971cbb76a11fc462697083ef1282b91d04
P. R. d. O <d.ol.rod@tutanota.com>
2022-10-13 18:57:31 -0600

    Performing a single query for the user id

--- sab/api/publishers/twitter.py
+++ sab/api/publishers/twitter.py
@@ -6,7 +6,7 @@ from typing import List
 
 class TwitterPublisher:
     client: Api = None
-    user: str = None
+    twitter_id: str = None
 
     __logger: Logger
 
@@ -16,8 +16,6 @@ class TwitterPublisher:
             consumer_secret = credentials["consumer_secret"]
             access_key = credentials["access_key"]
             access_secret = credentials["access_secret"]
-            if "user" in credentials:
-                self.user = credentials["user"]
             twitter = Api(
                 consumer_key=consumer_key,
                 consumer_secret=consumer_secret,
@@ -29,13 +27,23 @@ class TwitterPublisher:
             self.__logger = getLogger(("stream-alert-bot/api/publishers/" "twitter"))
             self.__logger.debug("Twitter connection generated")
             self.client = twitter
+            if "user" in credentials:
+                self.__logger.debug("Found user info on settings")
+                user_name = credentials["user"]
+                self.__logger.debug(" ".join(["Obtaining Twitter ID for user", user_name]))
+                lookup_result = self.client.UsersLookup(screen_name=user_name)
+                if len(lookup_result) == 0:
+                    raise ValueError("The user name list is empty.")
+                # We will always pick the first result
+                self.__logger.debug("Obtained user ID")
+                self.twitter_id = lookup_result[0].id
         except Exception as e:
             self.__logger.exception(e)
             sys.exit(1)
 
     def post_message(self, message: str) -> bool:
         try:
-            if self.user is None:
+            if self.twitter_id is None:
                 self.__logger.warn("User ID has not been provided. There could be duplicated tweets if you are using this on multiple instances.")
             elif self.__is_message_duplicated(message):
                 self.__logger.warn("Message is a duplicate. Not posting.")
@@ -53,12 +61,8 @@ class TwitterPublisher:
 
     def __is_message_duplicate(self, message: str) -> bool:
         try:
-            user = self.client.UsersLookup(screen_name=self.user)
-            if len(user) == 0:
-                raise ValueError("The user name list is empty.")
-            user_id = user[0].id
             # FIXME: See if it is needed to customize the count here
-            latest_tweets = self.client.GetUserTimeline(user_id=user_id,
+            latest_tweets = self.client.GetUserTimeline(user_id=self.twitter_id,
                                                         count=10,
                                                         include_rts=False,
                                                         trim_user=True,
